1a42631b6486b9810dd15b2420bd8f6fdb73e192
Fredy Augusto Castellon Bedoya <castellonbedoya@gmail.com>
2025-09-06 18:39:18 -0600

    CH3: Architecture - Monoliths and Decoupling

--- main.go
+++ main.go
@@ -21,11 +21,11 @@ func main()  {
 		fileserverHits: atomic.Int32{},
 	}
 
-	mux.HandleFunc("GET /healthz", handlerReadiness)
+	mux.HandleFunc("GET /api/healthz", handlerReadiness)
 
-	mux.HandleFunc("GET /metrics", apiCfg.handlerMetrics)
+	mux.HandleFunc("GET /api/metrics", apiCfg.handlerMetrics)
 
-	mux.HandleFunc("POST /reset", apiCfg.handlerReset)
+	mux.HandleFunc("POST /api/reset", apiCfg.handlerReset)
 
 	appHandler := http.StripPrefix("/app", http.FileServer(http.Dir(filepathRoot)))
 	mux.Handle("/app/", apiCfg.middlewareMetricsInc(appHandler))
